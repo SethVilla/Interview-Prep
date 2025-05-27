7a3efadeefe9d0bfb8f5fb95950c881d47e82efa
Seth Villa <sethvilla13@gmail.com>
2025-05-26 21:53:19 -0700

    wip

---
+++ grokking-the-coding-interview/two-pointers/palindrome-using-same-digits/solution.js
@@ -0,0 +1,23 @@
+export const findNextPermutation = () => {
+
+
+    return false
+}
+
+export const findNextPalindrome = (numStr) => {
+
+    const n = numStr.length
+
+    if (n === 1) {
+        return ""
+    }
+
+    let halfLength = Math.floor(n / 2)
+    let leftHalf = numStr.slice(0, halfLength).split()
+
+    if (!findNextPermutaion) {
+        return ""
+    }
+
+
+}
