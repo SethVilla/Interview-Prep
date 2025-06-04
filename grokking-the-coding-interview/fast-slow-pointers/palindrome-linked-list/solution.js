3e850afca85a5756e8cd812bf29b62e0a83f54c7
Seth Villa <sethvilla13@gmail.com>
2025-06-04 00:42:56 -0700

    inline solution for pali linked list

--- grokking-the-coding-interview/fast-slow-pointers/palindrome-linked-list/solution.js
+++ grokking-the-coding-interview/fast-slow-pointers/palindrome-linked-list/solution.js
@@ -38,3 +38,57 @@ const palindrome = (head) => {
     Time: O(n)
     Space: O(1)
  */
+
+
+function inlinePalindrome(head){
+    let slow = head
+    let fast = head
+
+    while (fast?.next) {
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    let next = null
+    let prev = null
+
+    while (slow) {
+        next = slow.next
+        slow.next = prev
+        prev = slow
+        slow = next
+    }
+
+    slow = prev
+    fast = head
+
+    while (slow && fast) {
+        if (slow.val !== fast.val) {
+            return false
+        }
+        slow = slow.next
+        fast = fast.next
+    }
+
+
+
+    slow = prev
+    prev = null
+    next = null
+
+    while (slow) {
+        next = slow.next
+        slow.next = prev
+        prev = slow
+        slow = next
+    }
+
+    while(head) {
+        console.log(head.val)
+        head = head.next
+    }
+
+
+    // Replace this placeholder return statement with your code
+    return true;
+}
