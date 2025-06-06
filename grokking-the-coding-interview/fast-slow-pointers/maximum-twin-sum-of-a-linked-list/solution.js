88aa707f604783e27566be65a9e9b38ebf5bdc9f
Seth Villa <sethvilla13@gmail.com>
2025-06-05 23:49:43 -0700

    running time + solution + anki card

--- grokking-the-coding-interview/fast-slow-pointers/maximum-twin-sum-of-a-linked-list/solution.js
+++ grokking-the-coding-interview/fast-slow-pointers/maximum-twin-sum-of-a-linked-list/solution.js
@@ -1,3 +1,45 @@
-export const twinSum = () => {
+const reverseLinkedList = (node) => {
+    let next = null
+    let prev = null
+    let curr = node
 
+    while(curr) {
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+
+    return prev
 }
+
+export const twinSum = (head) => {
+
+    let slow = head
+    let fast = head
+
+    while (fast?.next) {
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    let reversed = reverseLinkedList(slow)
+
+    let maxSum = 0
+    slow = reversed
+
+    while (head && slow) {
+        maxSum = Math.max(maxSum, head.val + slow.val)
+        head = head.next
+        slow = slow.next
+    }
+
+    reversed = reverseLinkedList(reversed)
+
+    return maxSum;
+}
+
+/*
+    Time:  O(n)
+    Space: O(1)
+ */
