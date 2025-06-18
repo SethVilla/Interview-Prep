999b5349295220ce4e6c1cef39387ab202f65f9b
Seth Villa <sethvilla13@gmail.com>
2025-06-17 23:30:13 -0700

    wip min window sub

--- grokking-the-coding-interview/sliding-window/minimum-window-subsequence/solution.js
+++ grokking-the-coding-interview/sliding-window/minimum-window-subsequence/solution.js
@@ -1,4 +1,17 @@
 
 export const minWindow = (st1, str2) => {
+    // save the size of str1 and str2
+    const sizeStr1 = str1.length;
+    const sizeStr2 = str2.length;
+
+    // initialize minSubLen to a very large number (infinity)
+    let minSubLen = Infinity;
+
+    // initialize pointers to zero and the minSubsequence to an empty string
+    let indexS1 = 0;
+    let indexS2 = 0;
+    let start = 0;
+    let minSubsequence = "";
+
     return ""
 }
